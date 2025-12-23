00b701113b510775ce6e246934806b9faf74002e
ADeveloperFromTurkey <yusuferdemongun@icloud.com>
2025-12-23 20:46:34 +0300

    Improvments

---
+++ NoktalaraYetis/MainMenu.swift
@@ -0,0 +1,8 @@
+//
+//  MainMenu.swift
+//  NoktalaraYetis
+//
+//  Created by Yusuf Erdem Ongun on 22.12.2025.
+//
+
+import Foundation
---
+++ NoktalaraYetis/OyunModSecim.swift
@@ -0,0 +1,8 @@
+//
+//  OyunModSecim.swift
+//  NoktalaraYetis
+//
+//  Created by Yusuf Erdem Ongun on 22.12.2025.
+//
+
+import Foundation
